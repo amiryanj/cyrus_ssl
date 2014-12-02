b71e63d71e877c0ca17e47bd91cc03bce3fda119
javad <amiryan.j@gmail.com>
2014-12-02 23:57:29 +0330

    proto files are added

---
+++ common/tools/SSLListener.cpp
@@ -0,0 +1,13 @@
+/*
+ * SSLListener.cpp
+ *
+ *  Created on: Aug 16, 2013
+ *      Author: mostafa
+ */
+
+#include "SSLListener.h"
+
+SSLListener::~SSLListener(){
+}
+
+
---
+++ common/tools/SSLListener.h
@@ -0,0 +1,21 @@
+/*
+ * SSLListener.h
+ *
+ *  Created on: Aug 16, 2013
+ *      Author: mostafa
+ */
+
+#ifndef SSLLISTENER_H_
+#define SSLLISTENER_H_
+
+class SSLListener {
+public:
+    virtual void check() = 0;
+	virtual ~SSLListener();   
+
+protected:
+
+
+};
+
+#endif /* SSLLISTENER_H_ */
