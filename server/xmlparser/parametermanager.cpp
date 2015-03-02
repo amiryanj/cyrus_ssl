c85ead85e7fe17101943658990876c4e21789993
Pouya Payandeh <pouya.payandeh@gmail.com>
2015-03-02 19:15:56 +0330

    XML PARSER ADDED TO SERVER

---
+++ server/xmlparser/parametermanager.cpp
@@ -0,0 +1,28 @@
+#include "parametermanager.h"
+#include <string>
+#include <locale>
+using namespace std;
+
+ParameterManager::ParameterManager()
+{
+    read_xml(filename, pt);
+}
+ParameterManager* ParameterManager::instance = NULL;
+const string ParameterManager::filename = "Setting.xml";
+ParameterManager *ParameterManager::getInstance()
+{
+    if(instance == NULL)
+        instance = new ParameterManager();
+    return instance;
+}
+
+void ParameterManager::beginGroup(string group)
+{
+    groups.push_back(group);
+}
+
+void ParameterManager::endGroup()
+{
+
+    groups.pop_back();
+}
---
+++ server/xmlparser/parametermanager.h
@@ -0,0 +1,49 @@
+#ifndef PARAMETERMANAGER_H
+#define PARAMETERMANAGER_H
+#include <iostream>
+#include <string>
+#include <vector>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/xml_parser.hpp>
+#include <locale>
+#include <sstream>
+#include <iostream>
+
+using namespace std;
+class ParameterManager
+{
+private:
+    static const string filename ;
+    //IniParser parser;
+
+    boost::property_tree::ptree pt;
+
+    static ParameterManager* instance;
+    vector<string> groups;
+    ParameterManager();
+    std::string toLower(std::string str)
+    {
+        std::string out;
+        std::locale loc;
+        for (unsigned int i=0; i<str.length(); ++i)
+            out += std::tolower(str[i],loc);
+       // std::cout << out << std::endl;
+        return out;
+    }
+public :
+    static ParameterManager *getInstance();
+    void beginGroup(string group);
+    void endGroup();
+    template <typename T> T get ( std::string key)
+    {
+        string global_key;
+        for(int i = 0 ; i < groups.size() ;  i++)
+            global_key+=toLower(groups[i])+".";
+        global_key+=toLower(key);
+        return pt.get<T>(global_key);
+
+    }
+
+};
+
+#endif // PARAMETERMANAGER_H
