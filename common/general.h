6868c83d317797aa81149c5f86210c22f5af189b
Javad <amiryanj@ce.sharif.edu>
2015-04-01 01:02:03 +0430

    Field Size updated to meet SSL Rules 2015

--- common/general.h
+++ common/general.h
@@ -15,13 +15,13 @@
 #define SERIAL_PORT "/dev/ttyUSB0"
 #define SERIAL_BAUD 115200
 
-#define FIELD_WIDTH                 4050.0
-#define FIELD_LENGTH                6050.0
-#define FIELD_CENTER_RADIUS         500.0
-#define FIELD_PENALTY_AREA_RADIUS   900.0
-#define FIELD_PENALTY_AREA_WIDTH    350.0
-#define FIELD_GOAL_WIDTH            700.0
-#define FIELD_PENALTY_DISTANCE      750.0
+#define FIELD_WIDTH                 6000.0
+#define FIELD_LENGTH                9000.0
+#define FIELD_CENTER_RADIUS         1000.0
+#define FIELD_PENALTY_AREA_RADIUS   1000.0
+#define FIELD_PENALTY_AREA_WIDTH    500.0
+#define FIELD_GOAL_WIDTH            1000.0
+#define FIELD_PENALTY_DISTANCE      1000.0
 
 #define ROBOT_RADIUS        90.0
 #define BALL_RADIUS         21.5
