5001c7b60aa03b509ae4a3d313f444c89b190302
Javad <amiryanj@ce.sharif.edu>
2015-04-01 01:52:14 +0430

    git ignores temporal changes in setting files

--- visualizer/mainwindow.cpp
+++ visualizer/mainwindow.cpp
@@ -61,7 +61,7 @@ MainWindow::MainWindow(Color our_color, Side our_side, QWidget *parent) :
     ui->actionShow_Targets->setChecked(true);
     ui->actionConnect_Server->setChecked(true);
 
-    ui_settings = new QSettings("../settings/ui_settings.ini", QSettings::IniFormat, this);
+    ui_settings = new QSettings("../../cyrus2014/settings/ui_settings.ini", QSettings::IniFormat, this);
     ui->splitter_1->restoreState(ui_settings->value("splitter_1").toByteArray());
     ui->splitter_2->restoreState(ui_settings->value("splitter_2").toByteArray());
 
